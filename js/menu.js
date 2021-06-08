86e71be149ac16543c160e3458e4981118775cbf
Samuel <samupascua@gmail.com>
2021-06-08 18:54:27 +0200

    Modificación a los js debido a que fallaban los dos a la vez

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,14 @@
-window.onload = main;
+const icono = document.getElementById('icono-menu');
+const menu = document.getElementById('menu-navegacion');
 
-function main(){
-    const icono = document.getElementById('icono-menu');
-    const menu = document.getElementById('menu-navegacion');
+icono.addEventListener('click', ()=>{
+    menu.classList.toggle("mostrar");
+    icono.classList.toggle("ocultar");
+})
 
-    icono.addEventListener('click', ()=>{
+window.addEventListener('click', (e)=>{
+    if(menu.classList.contains('mostrar') && e.target != menu && e.target != icono){
         menu.classList.toggle("mostrar");
         icono.classList.toggle("ocultar");
-    })
-
-
-    window.addEventListener('click', (e)=>{
-        if(menu.classList.contains('mostrar') && e.target != menu && e.target != icono){
-            menu.classList.toggle("mostrar");
-            icono.classList.toggle("ocultar");
-        }
-    })
-}
+    }
+})
